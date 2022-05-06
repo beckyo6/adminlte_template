2928a257c0dd18d28869de48471b539502997a16
Becky Ada <rebtshikadila@gmail.com>
2022-05-06 06:04:47 +0200

    add card to index view

--- resources/views/back/index.blade.php
+++ resources/views/back/index.blade.php
@@ -1,3 +1,47 @@
 @extends('back.layout')
 @section('content')
+    <div class="row col-lg-12">
+        <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-white text-center">
+                <div class="inner">
+                    <h3>150</h3>
+                    <p>New Orders</p>
+                </div>
+            </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-white text-center">
+                <div class="inner">
+                    <h3>53</h3>
+                    <p>Bounce Rate</p>
+                </div>
+            </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-white text-center">
+                <div class="inner">
+                    <h3>44</h3>
+                    <p>User Registrations</p>
+                </div>
+            </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-white text-center">
+                <div class="inner">
+                    <h3>65</h3>
+
+                    <p>Unique Visitors</p>
+                </div>
+                
+            </div>
+        </div>
+        <!-- ./col -->
+    </div>
 @endsection
