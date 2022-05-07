b775eec4d1c79cedbce21cab4dd18bd92d0b90d5
Becky Ada <rebtshikadila@gmail.com>
2022-05-07 08:34:13 +0200

    added the route on the ajout d'un admin in the menu

--- resources/views/back/layout.blade.php
+++ resources/views/back/layout.blade.php
@@ -91,7 +91,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                             </a>
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
-                                    <a href="#" class="nav-link">
+                                    <a href="{{ route('admins.create')}}" class="nav-link">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>ajouter un admin</p>
                                     </a>
